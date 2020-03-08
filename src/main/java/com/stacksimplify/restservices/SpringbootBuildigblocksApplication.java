44c2449105a1a0cf50dcbcd80d65c1463bb71474
Natarajan Swaminathan <snatrajs@gmail.com>
2020-03-08 12:18:17 +0530

    First Commit Changes

---
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildigblocksApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildigblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildigblocksApplication.class, args);
+	}
+
+}
